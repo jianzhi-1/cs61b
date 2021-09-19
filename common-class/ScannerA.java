18354f6a0dbffd30f14dfee1bfc136d7f0b50b50
Jianzhi Wang <jianzhiw00@gmail.com>
2021-09-18 21:52:45 -0700

    Update and rename AScanner.java to ScannerA.java

--- common-class/ScannerA.java
+++ common-class/ScannerA.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AScanner {
+public class ScannerA {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String input = s.nextLine(); // get a line
