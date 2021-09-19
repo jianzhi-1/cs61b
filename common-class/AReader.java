4c082f75e45d6a19ba48d7c11e539f6f4809ca4b
Jianzhi Wang <jianzhiw00@gmail.com>
2021-09-18 21:41:35 -0700

    Create AReader.java

---
+++ common-class/AReader.java
@@ -0,0 +1,17 @@
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class AReader{
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        Reader r = new FileReader("file.txt");
+        int c = r.read(); // returns an int containing the char value of next character read
+        while (c != -1){
+            System.out.print((char)c);
+            c = r.read();
+        }
+        System.out.println();
+    }
+}
+
