c481b8708ce98edd5458332d84dae55f93390eb5
Jianzhi Wang <jianzhiw00@gmail.com>
2021-09-18 21:45:21 -0700

    Create BReader.java

---
+++ common-class/BReader.java
@@ -0,0 +1,17 @@
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class BReader{
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        Reader r = new FileReader("file.txt");
+        char[] charr = new char[128];
+        int c = r.read(charr, 0, charr.length);
+        while (c != -1){
+            System.out.println(new String(charr, 0, c));
+            c = r.read(charr, 0, charr.length);
+        }
+    }
+}
+
