93d430eb4d4d7e12bb2b6b0d8c7492c540c6ef75
Jianzhi Wang <jianzhiw00@gmail.com>
2021-09-18 21:53:18 -0700

    Update and rename AReader.java to ReaderA.java

--- common-class/ReaderA.java
+++ common-class/ReaderA.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class AReader{
+public class ReaderA {
     public static void main(String[] args) throws FileNotFoundException, IOException{
         Reader r = new FileReader("file.txt");
         int c = r.read(); // returns an int containing the char value of next character read
